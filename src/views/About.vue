321e90d33c1f140f9356eeaf7b9dd134a4584300
opvelll <opvel.hero@gmail.com>
2020-08-19 02:43:58 +0900

    [add] aboutの変更

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about d-flex flex-column align-items-center">
-    <span>死までの日数を表示できます。</span>
+    <span>100日後まであと何日か表示します。</span>
     <span>元ネタ: 100日後に死ぬワニ</span>
   </div>
 </template>
